5d603b47a8381040a0c67017467c17d4f1080654
Jonathan Hernández <aggressivepixels@gmail.com>
2021-06-27 18:46:08 -0400

    Hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jisho Download Audio
+
+This small extension adds a "Download audio" button to Jisho's entries if
+they contain audio. Unfortunately, it doesn't _actually_ trigger a download
+(at least in Firefox 89) but you can right-click it and download it that way.
---
+++ add-download-button.js
@@ -0,0 +1,13 @@
+document.querySelectorAll("a.concept_audio").forEach(function (el) {
+  var id = el.getAttribute("data-id");
+  var audio = document.getElementById(id);
+  var source = audio.querySelector('source[type="audio/mpeg"]');
+
+  var download = document.createElement("a");
+  download.className = "concept_light-status_link";
+  download.download = source.src.split("/").pop();
+  download.href = source.src;
+  download.innerHTML = "Download audio";
+
+  el.parentNode.insertBefore(download, el.nextSibling);
+});
